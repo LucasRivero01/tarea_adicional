dcd2303bcd04c2f00dfa20f6eeca2232e44eda07
LucasRivero01 <lucas.rivero@naranjax.com>
2022-01-26 20:04:57 -0300

    se resuelven errores con eslint

--- calculos.js
+++ calculos.js
@@ -1,28 +1,18 @@
 const fs = require('fs');
 const { validarEdad } = require('./config/validaciones');
-var color = require('colors');
-const crearArchivo = (nombre, apellido, edad) => {
-    return new Promise((resolve, reject) => {
-        let resultado = '';
-        let salidaConsola = '';
-        // let nombreArch = '';
-        // if (edad >= 45) {
-        //     nombreArch = './salida/mayor45.txt'
-        // } else {
-        //     nombreArch = './salida/menor45.txt'
-        // }
-        const nombreArch = validarEdad(edad);
-        //\n para salto de linea
-        resultado = `${nombre},${apellido},${edad},\n`;
 
+const crearArchivo = (nombre, apellido, edad) => new Promise((resolve) => {
+  let resultado = '';
+  const nombreArch = validarEdad(edad);
 
-        fs.writeFile(nombreArch, resultado, { flag: 'a+' }, (err) => {
-            if (err) throw err;
-        })
-        resolve(nombreArch);
-    })
-}
+  resultado = `${nombre},${apellido},${edad},\n`;
+
+  fs.writeFile(nombreArch, resultado, { flag: 'a+' }, (err) => {
+    if (err) throw err;
+  });
+  resolve(nombreArch);
+});
 
 module.exports = {
-    crearArchivo,
-}
+  crearArchivo,
+};
--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const argv = require('./config/yargs');
 const { lectura } = require('./lectura');
-const { crearArchivo } = require('./calculos')
-
+const { crearArchivo } = require('./calculos');
 
 if (argv.d) {
-    const leoArchivo = lectura(argv.n, argv.a, argv.e)
-        .then(nombreArchivo => console.log(nombreArchivo, 'generado'))
-        .catch(err => console.log(err));
+  lectura(argv.n, argv.a, argv.e)
+    .then((nombreArchivo) => console.log(nombreArchivo, 'generado'))
+    .catch((err) => console.log(err));
 } else {
-    const creoArchivo = crearArchivo(argv.n, argv.a, argv.e)
-        .then(nombreArchivo => console.log(nombreArchivo, 'actualizado'))
-        .catch(err => console.log(err));
+  crearArchivo(argv.n, argv.a, argv.e)
+    .then((nombreArchivo) => console.log(nombreArchivo, 'actualizado'))
+    .catch((err) => console.log(err));
 }
